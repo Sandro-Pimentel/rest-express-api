d596eb340c4c5f22663e11cc41abf9018402b8e0
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2024-10-04 19:54:02 -0300

    feat: Creating the user table
    Creating the user table with sequelize.

--- config/config.js
+++ config/config.js
@@ -7,20 +7,20 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres'
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
     dialect: "postgres"
   },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'postgres'
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   }
 }
